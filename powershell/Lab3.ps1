fdcb974ae5eff7536ba3f0ebfe2c6367f99a1407
Jacob T. Smith <108315224+Jacob1e@users.noreply.github.com>
2023-02-05 23:44:31 -0600

    Add files via upload

---
+++ powershell/Lab3.ps1
@@ -0,0 +1,16 @@
+﻿function getIP{
+(get-netipaddress).ipv4address | Select-String "192*"
+}
+
+$IP = getIP
+$USER = $env:Username
+$HOSTNAME = $env:computername
+$PS = $HOST.Version.Major
+$Date = Get-Date -UFormat "%A, %B %d %Y"
+
+Write-Host($BODY)
+
+
+$BODY = ("This machines IP is $IP. The user is $USER. Hostname is $HOSTNAME. Powershell version is $PS. The date is $Date")
+
+Send-MailMessage -To "Smit9j4@mail.uc.edu" -From "Jacobfn8@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
