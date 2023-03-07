5c58fdc12f132bcb304e130b06bb4da2d147291f
Jacob T. Smith <108315224+Jacob1e@users.noreply.github.com>
2023-03-06 20:01:22 -0500

    Update Lab7.py

--- Labs/Lab7.py
+++ Labs/Lab7.py
@@ -1,20 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Send request to website
 response = requests.get("https://www.example.com")
 
-# Parse HTML
 soup = BeautifulSoup(response.content, "html.parser")
 
-# Extract title of the website
 title = soup.find("title").text
 print(f"Website Title: {title}")
 
-# Extract all the links on the page
 links = [link.get("href") for link in soup.find_all("a")]
 print("Links: ", links)
 
-# Extract all the headings on the page
 headings = [heading.text for heading in soup.find_all(["h1", "h2", "h3"])]
 print("Headings: ", headings)
